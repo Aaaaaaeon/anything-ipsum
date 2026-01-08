c071df032ed82143687b5e8815787dc1557e0180
Aeon <99080267+Aaaaaaeon@users.noreply.github.com>
2026-01-08 10:41:30 +0100

    fix: separate Jest and Karma test configs, run API tests in CI

--- jest.api.config.js
+++ jest.api.config.js
@@ -7,7 +7,7 @@ module.exports = {
   moduleFileExtensions: ['ts', 'js', 'json'],
   transform: {
     '^.+\\.ts$': ['ts-jest', {
-      tsconfig: 'tsconfig.spec.json',
+      tsconfig: 'tsconfig.jest.json',
     }],
   },
 };
